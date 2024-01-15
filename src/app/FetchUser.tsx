01abf933a814925467699d67fe9a0968c04ebfcf
anish-singh-rawat <anishsinghrawat1>
2024-01-15 12:26:42 +0530

    crud operation

---
+++ src/app/FetchUser.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { CircularProgress } from "@mui/material";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const FetchUser = () => {
+  const [users, setUsers] = useState<any>([]);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/api/auth/Getusers");
+      setUsers(response.data.users);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  return (
+    <>
+      <div>
+        {
+          users.length <1 ?  
+          <div className="mt-52">
+            <CircularProgress/> 
+          </div>
+         :
+          users.map((item: { _id: string, name: string, email: string, password : string }) => (
+          <div key={item._id} className="flex flex-col mt-6 p-4 border rounded-md bg-gray-100">
+            <div className="flex justify-between ">
+            <div className="mt-3 text-xl font-semibold">Name : {item.name}</div>
+            <div className="mt-3 text-xl font-semibold bg-orange-400 border rounded-md cursor-pointer">Edit</div>
+            </div>
+            <div className="flex justify-between">
+            <div className="mt-3 text-xl font-semibold">Email : {item.email}</div>
+            <div className="mt-3 text-xl font-semibold bg-orange-400 border rounded-md cursor-pointer">Edit</div>
+            </div>
+          </div>
+        ))
+        
+        }
+      </div>
+    </>
+  );
+};
+
+export default FetchUser;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,39 @@ import Logout from "@/components/Logout";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/options";
 import { redirect } from "next/navigation";
+import FetchUser from "./FetchUser";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-  if(!session){
-    redirect('/auth/login')
+  interface User {
+    name: string;
+    email: string;
   }
-  
+  const session = await getServerSession(authOptions);
+  const curentUser = JSON.stringify(session);
+  const currentUserObj = JSON.parse(curentUser);
+  const currentUserArr: User[] = Object.values(currentUserObj);
+  console.log(session,'ddddddd');
+  if (!session) {
+    redirect("/auth/login");
+  }
   return (
     <>
-    <div>
-      <div className="flex justify-center items-center h-screen flex-col">
-        <h1 className="bg-purple-500  text-3xl font-bold mb-6">
-          hello User welcome to your personal account and your website !
-        </h1>
-        <div> {JSON.stringify(session)} </div>
-      <Logout/>
+      <div className="mt-6">
+        <div className="flex justify-center items-center flex-col">
+          <h1 className="bg-purple-500  text-3xl font-bold mb-6">
+            Welcome in Admin page
+          </h1>
+          <div className="font-bold">
+            Hello MR : {currentUserArr[0].name}
+            <br />
+            Email ID : {currentUserArr[0].email}
+          </div>
+          <Logout />
+          <div className="h-96 overflow-y-scroll mt-5">
+            <FetchUser />
+          </div>
+        </div>
       </div>
-    </div>
     </>
-    )
+  );
 }
